557daf6ee7abe3001c8f6c20469c9360d7ce9d37
filipw <wojcieszyn@gmail.com>
2018-12-04 15:30:24 +0100

    updated the demo

--- CompletionDemo/CompletionDemo/Program.cs
+++ CompletionDemo/CompletionDemo/Program.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Completion;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.Text;
 using System;
 using System.Composition.Hosting;
 using System.Linq;
-using System.Reflection;
+using System.Threading.Tasks;
 
 namespace CompletionDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        async static Task Main(string[] args)
         {
-            // default asseblies are
+            // default assemblies are
             //    "Microsoft.CodeAnalysis.Workspaces",
             //    "Microsoft.CodeAnalysis.CSharp.Workspaces",
             //    "Microsoft.CodeAnalysis.VisualBasic.Workspaces",
@@ -28,10 +31,75 @@ namespace CompletionDemo
                 .CreateContainer();
 
             var host = MefHostServices.Create(MefHostServices.DefaultAssemblies);
-
             var workspace = new AdhocWorkspace(host);
 
-            Console.WriteLine("Hello World!");
+            var code = @"using System;
+
+            public class MyClass
+            {
+                public static void MyMethod(int value)
+                {
+                    Guid.
+                }
+            }";
+
+            var projectInfo = ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Create(), "MyProject", "MyProject", LanguageNames.CSharp).
+                WithMetadataReferences(new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) });
+            var project = workspace.AddProject(projectInfo);
+            var document = workspace.AddDocument(project.Id, "MyFile.cs", SourceText.From(code));
+
+            await PrintCompletionResults(document, code.LastIndexOf("Guid.") + 5);
+
+            Console.WriteLine();
+            Console.WriteLine("*****");
+            Console.WriteLine();
+            
+            // SCRIPT VERSION
+
+            var scriptCode = "Guid.N";
+
+            var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary,
+                usings: new[] { "System" });
+            var parseOptions = new CSharpParseOptions(LanguageVersion.Latest, DocumentationMode.Parse, SourceCodeKind.Script);
+            var scriptProjectInfo = ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Create(), "Script","Script", LanguageNames.CSharp,
+                    compilationOptions: compilationOptions, parseOptions: parseOptions, isSubmission: true)
+                .WithMetadataReferences(new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) });
+
+            var scriptProject = workspace.AddProject(scriptProjectInfo);
+            var scriptDocumentInfo = DocumentInfo.Create(
+                DocumentId.CreateNewId(scriptProject.Id), Guid.NewGuid() + ".csx",
+                sourceCodeKind: SourceCodeKind.Script,
+                loader: TextLoader.From(TextAndVersion.Create(SourceText.From(scriptCode), VersionStamp.Create())));
+            var scriptDocument = workspace.AddDocument(scriptDocumentInfo);
+
+            await PrintCompletionResults(scriptDocument, scriptCode.Length - 1);
+
+            Console.ReadLine();
+        }
+
+        private static async Task PrintCompletionResults(Document document, int position)
+        {
+            var completionService = CompletionService.GetService(document);
+            var results = await completionService.GetCompletionsAsync(document, position);
+
+            foreach (var i in results.Items.Where(x => !x.Tags.Contains("Keyword")))
+            {
+                Console.WriteLine(i.DisplayText);
+
+                foreach (var prop in i.Properties)
+                {
+                    Console.Write($"{prop.Key}:{prop.Value}  ");
+                }
+
+                Console.WriteLine();
+                foreach (var tag in i.Tags)
+                {
+                    Console.Write($"{tag}  ");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+            }
         }
     }
 }
